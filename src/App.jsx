954c8f6fea7474663a9897ff5e2e5cb85c418043
gihozo-elyse <gihozoelyse@gmail.com>
2025-10-19 17:34:51 +0700

    create responsive About page with mission and features grid

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import Home from "./pages/Home.jsx"
 import Products  from './pages/Products.jsx'
+import About from './About.jsx'
 
 function App() {
   return (
@@ -15,6 +16,8 @@ function App() {
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/products" element={<Products />} />
+              <Route path="/About" element={<About />} />
+
             </Routes>
           </main>
         </div>
