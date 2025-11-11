a080b9e4d2eb302d9e262736cd244e64bf5aade2
gihozo-elyse <gihozoelyse@gmail.com>
2025-11-11 16:44:54 +0700

    fix: resolve vite build path issue

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  },
+  resolve: {
+    alias: {
+      '/src': '/src'
+    }
+  },
+  base: './', 
 })
-
